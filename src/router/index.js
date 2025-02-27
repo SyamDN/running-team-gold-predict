84693bbcce1c12234fff873ef0f9ea62aa285254
aaneira <aaneirra@gmail.com>
2025-02-27 19:52:59 +0700

    Update Dokumentasi index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,28 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from '../views/Home.vue';
-import ArtikelEmas from "@/views/ArtikelEmas.vue";
-import SimulasiPrediksi from "@/views/SimulasiPrediksi.vue";
+import { createRouter, createWebHistory } from "vue-router"; 
+import Home from '../views/Home.vue'; 
+import ArtikelEmas from "@/views/ArtikelEmas.vue"; 
+import SimulasiPrediksi from "@/views/SimulasiPrediksi.vue"; 
 
+// Definisi daftar rute dalam aplikasi
 const routes = [
     {
-        path: '/', component: Home
+        path: '/', // Rute utama (halaman beranda)
+        component: Home
     },
     {
-        path: '/artikel/:id',
+        path: '/artikel/:id', // Rute untuk halaman artikel emas dengan parameter ID
         component: ArtikelEmas
     },
     {
-        path: '/simulasi', component: SimulasiPrediksi
+        path: '/simulasi', // Rute untuk halaman simulasi prediksi harga emas
+        component: SimulasiPrediksi
     },
 ];
 
+// Membuat instance router dengan mode history
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+    history: createWebHistory(), // Menggunakan mode history untuk URL yang lebih bersih (tanpa hash "#")
+    routes, // Memasukkan daftar rute yang telah didefinisikan
 });
 
-export default router;
+export default router; // Mengekspor router agar bisa digunakan dalam aplikasi Vue
